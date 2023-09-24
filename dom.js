0597714b9f7a85b78a26ac8d81fde526855a905f
urvashipatill <urvashimurlipatil23@gmail.com>
2023-09-25 01:55:55 +0530

    li updated

--- dom.js
+++ dom.js
@@ -1,15 +1,24 @@
-document.title= 'My World'
-var headerTitle= document.getElementById('main-header')
-// console.log(headerTitle)
-headerTitle.style.borderBottom = 'solid 3px #000'
-var additems= document.getElementById('additems')
-additems.style.color= 'green'
-additems.style.fontWeight= 'bold'
-const listItem= document.getElementsByClassName("list-group-item")
-listItem[2].style.backgroundColor= 'green'
-let l= Array.from(listItem)
-for(var i=0; i<l.length;i++){
-    if(i!==2){
-        l[i].style.fontWeight='bold'
-    }
-}
+// document.title= 'My World'
+// var headerTitle= document.getElementById('main-header')
+// headerTitle.style.borderBottom = 'solid 3px #000'
+// var additems= document.getElementById('additems')
+// additems.style.color= 'green'
+// additems.style.fontWeight= 'bold'
+// const listItem= document.getElementsByClassName("list-group-item")
+// listItem[2].style.backgroundColor= 'green'
+// let l= Array.from(listItem)
+// for(var i=0; i<l.length;i++){
+//     if(i!==2){
+//         l[i].style.fontWeight='bold'
+//     }
+// }
+
+    var ul= document.getElementById('items')
+    var li= document.createElement('li')
+    li.appendChild(document.createTextNode("Item 5"))
+    ul.appendChild(li)
+    li.className= "newlist"
+    li.tagName="listcreated"
+ console.log(document.getElementsByClassName("newlist"))
+ console.log(document.getElementsByTagName("listcreated"))
+ 
