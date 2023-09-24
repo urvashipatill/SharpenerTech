6854f2766fcad99b0833b99bfb9186fb663f689b
urvashipatill <urvashimurlipatil23@gmail.com>
2023-09-25 00:16:18 +0530

    list updated

--- dom.js
+++ dom.js
@@ -1,7 +1,15 @@
 document.title= 'My World'
 var headerTitle= document.getElementById('main-header')
-console.log(headerTitle)
+// console.log(headerTitle)
 headerTitle.style.borderBottom = 'solid 3px #000'
 var additems= document.getElementById('additems')
 additems.style.color= 'green'
 additems.style.fontWeight= 'bold'
+const listItem= document.getElementsByClassName("list-group-item")
+listItem[2].style.backgroundColor= 'green'
+let l= Array.from(listItem)
+for(var i=0; i<l.length;i++){
+    if(i!==2){
+        l[i].style.fontWeight='bold'
+    }
+}
