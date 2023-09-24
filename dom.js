c6a58831ea62f3f41209a832cbb5d3adce31825b
urvashipatill <urvashimurlipatil23@gmail.com>
2023-09-24 23:03:01 +0530

    Adding Code

---
+++ dom.js
@@ -0,0 +1,7 @@
+document.title= 'My World'
+var headerTitle= document.getElementById('main-header')
+console.log(headerTitle)
+headerTitle.style.borderBottom = 'solid 3px #000'
+var additems= document.getElementById('additems')
+additems.style.color= 'green'
+additems.style.fontWeight= 'bold'
