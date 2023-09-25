8bdbdd7beddee2faccd84695e6ad4232928db71e
urvashipatill <urvashimurlipatil23@gmail.com>
2023-09-25 19:53:35 +0530

    Prt 2 updated

--- dom.js
+++ dom.js
@@ -1,37 +1,39 @@
-// document.title= 'My World'
-// var headerTitle= document.getElementById('main-header')
-// headerTitle.style.borderBottom = 'solid 3px #000'
-// var additems= document.getElementById('additems')
-// additems.style.color= 'green'
-// additems.style.fontWeight= 'bold'
-// const listItem= document.getElementsByClassName("list-group-item")
-// listItem[2].style.backgroundColor= 'green'
-// let l= Array.from(listItem)
-// for(var i=0; i<l.length;i++){
-//     if(i!==2){
-//         l[i].style.fontWeight='bold'
-//     }
-// }
+var listitem= document.querySelector("#items")
 
-//     var ul= document.getElementById('items')
-//     var li= document.createElement('li')
-//     li.appendChild(document.createTextNode("Item 5"))
-//     ul.appendChild(li)
-//     li.className= "newlist"
-//     li.tagName="listcreated"
-//  console.log(document.getElementsByClassName("newlist"))
-//  console.log(document.getElementsByTagName("listcreated"))
- 
-//Make second item have green bg and 3 item invisible
-// let list= document.getElementsByClassName("list-group-item")
-// list[1].style.backgroundColor= 'green'
-// list[2].style.display= 'none'
+// PARENT ELEMENT AND PARENT NODE
+console.log(listitem.parentNode)
+// listitem.parentNode.style.backgroundColor= '#f4f4f4'
+console.log(listitem.parentNode.parentElement)
+console.log(listitem.parentNode.parentNode)
+console.log(listitem.parentElement.style.backgroundColor= '#f3f3f2')
 
-// make 2nd list text green and odd no background green 
-let x=document.querySelectorAll(".list-group-item")
-x[1].style.color='green'
-var odd= document.querySelectorAll('li:nth-child(odd)')
-console.log(odd)
-for(var i=0; i<odd.length;i++){
-    odd[i].style.backgroundColor='green'
-}
+// CHILD NODE
+console.log(listitem.childNodes)
+console.log(listitem.children)
+console.log(listitem.children[1])
+listitem.children[1].style.backgroundColor= 'grey'
+listitem.children[3].style.backgroundColor='grey'
+
+// FIRST ELEMENT CHILD OR FIRST CHILD
+listitem.firstElementChild.style.backgroundColor='#f4f4f4'
+console.log(listitem.firstChild)
+
+// LAST ELEMENT CHILD
+console.log(listitem.lastElementChild)
+listitem.lastElementChild.textContent='Hey'
+
+// NEXT SIBLING
+console.log(listitem.nextSibling)
+console.log(listitem.nextElementSibling)
+
+// CREATE CHILD OR CREATE ELEMENT
+var newDiv= document.createElement('div')
+newDiv.className='newdiv'
+newDiv.id='Hello1'
+newDiv.setAttribute('name', 'Hello world!')
+console.log(newDiv)
+var newDivtext=document.createTextNode('Hello World')
+newDiv.appendChild(newDivtext)
+var container= document.querySelector('header .container')
+var h1= document.querySelector('header h1')
+container.insertBefore(newDiv, h1)
