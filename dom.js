690b61d2d9ff7c9596ede9013e1f38c02db2859d
urvashipatill <urvashimurlipatil23@gmail.com>
2023-09-26 22:12:48 +0530

    add filter and add description

--- dom.js
+++ dom.js
@@ -1,10 +1,14 @@
 let form= document.getElementById("addForm")
 let list= document.getElementById('items')
+var filter= document.getElementById('filter')
 // ADD ITEM OR RESPONSE ON SUBMIT BUTTON 
  form.addEventListener('submit', addItems)
 
 //  REMOVE ITEM OR RESPONSE ON DELETE CLICK 
 list.addEventListener('click', removeItem)
+
+// FILTER EVENT 
+filter.addEventListener('keyup', filterItems)
 
 
 //  CREATING FUNCTION addITems 
@@ -12,11 +16,14 @@ list.addEventListener('click', removeItem)
     e.preventDefault()
 // GET THE INPUT VALUE 
     let input= document.getElementById('item').value
+    let description= document.getElementById('describe').value
 // CREATE li ELEMENT
     var li= document.createElement('li')
     li.className ="list-group-item"
+
 // CREATE THE INPUT IN FORM OF TEXT NODE AND SAVE IT IN OUR NEW ELEM li 
     li.appendChild(document.createTextNode(input))
+    li.appendChild(document.createTextNode(" "+description))
     list.appendChild(li)
 // CREATE THE DELETE BTN FOR NEW ELEM
     let deleteBtn= document.createElement('button')
@@ -35,4 +42,25 @@ if(e.target.classList.contains('delete')){
         list.removeChild(li)
     }
 }
+}
+// CREATING FILTER EVENT LISTENER 
+
+function filterItems(e){
+// CONVERT IT INTO LOWERCASE 
+var text= e.target.value.toLowerCase();
+// GET list 
+var lists=list.getElementsByTagName('li')
+
+// CONVERT TO AN ARRAY 
+Array.from(lists).forEach(function (item) {
+let itemName= item.firstChild.textContent;
+let describeName= item.childNodes[1].textContent;
+if(itemName.toLowerCase().indexOf(text) !=-1 || describeName.toLowerCase().indexOf(text) !=-1){
+item.style.display= 'block'
+}    
+else{
+    item.style.display='none'
+}
+
+});
 }
