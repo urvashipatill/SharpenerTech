0eeae049c4988ec7007bb647e53f246e4fd56d69
urvashipatill <145540461+urvashipatill@users.noreply.github.com>
2023-11-19 21:11:48 +0530

    Add files via upload

--- appointment.js
+++ appointment.js
@@ -8,6 +8,8 @@ function additems(){
 let inputname= document.getElementById('username').value
 let inputnumber= document.getElementById('userno').value
 let inputmail= document.getElementById('usermail').value
+
+
 // storage 
 let obj={
    Name:inputname,
@@ -17,15 +19,21 @@ let obj={
 }
 let newData= JSON.stringify(obj)
 
-console.log(obj)
-   axios.post('https://crudcrud.com/api/ec2835c33e144026af307052d2c73666/appointdata',obj)
+// posting it into axios 
+
+   axios.post('https://crudcrud.com/api/00026c8cf007492facf10f8a5d21fc86/appointdata',obj)
    .then((response) =>{
+      console.log(response.data._id)
+      console.log(response.data)
    showonScreen(response.data)
    })
    .catch((err) =>{
    console.log(err)
    })
 
+}
+  
+   // show on screen 
    function showonScreen(e)
    {
    let li= document.createElement('li')
@@ -44,13 +52,10 @@ console.log(obj)
       {
           let li= e.target.parentElement
           list.removeChild(li)
-         
-          
+          console.log(li)
           
       }
-          
        }
-
        editBtn.onclick=(e) =>{
          confirm('Do you want to make chages?')
              {   let li=e.target.parentElement
@@ -60,72 +65,33 @@ console.log(obj)
                  document.getElementById('usermail').value=Inpemail
              }
           }
-
     li.appendChild(document.createTextNode(e.Name))
     li.appendChild(document.createTextNode(" "+ e.Number))
     li.appendChild(document.createTextNode(" "+ e.Email))
+    li.appendChild(document.createTextNode(" "+e._id))
     li.appendChild(removeBtn)
     li.appendChild(editBtn)
     list.appendChild(li)
-   
-   }
 
+   }
    document.getElementById('username').value=""
    document.getElementById('userno').value=""
-   document.getElementById('usermail').value=""
+   document.getElementById('usermail').value=""   
 
-   
-}
-axios.get('https://crudcrud.com/api/ec2835c33e144026af307052d2c73666/appointdata')
+   axios.get('https://crudcrud.com/api/00026c8cf007492facf10f8a5d21fc86/appointdata')
    .then((e) =>{
+      
       let arr=e.data
       for ( i=0; i<arr.length;i++) {
-         let li= document.createElement('li')
-         li.className='list-group-item'
-     
-         let removeBtn= document.createElement('button')
-         removeBtn.textContent='REMOVE'
-         removeBtn.id='remove'
-     
-         let editBtn=document.createElement('button')
-         editBtn.textContent='EDIT'
-         editBtn.id='edit'
-        
-
-         removeBtn.onclick=(e) =>{
-            confirm('Do you want to delete?')
-            {
-                let li= e.target.parentElement
-                list.removeChild(li)
-               
-                
-                
-            }
-                
-             }
-      
-             editBtn.onclick=(e) =>{
-               confirm('Do you want to make chages?')
-                   {   let li=e.target.parentElement
-                    list.removeChild(li)
-                       document.getElementById('username').value=Inpname
-                       document.getElementById('userno').value=Inpnumber
-                       document.getElementById('usermail').value=Inpemail
-                   }
-                }
-         
-         li.appendChild(document.createTextNode(arr[i].Name))
-         li.appendChild(document.createTextNode(" "+ arr[i].PhoneNumber))
-         li.appendChild(document.createTextNode(" "+ arr[i].Email))
-         li.appendChild(removeBtn)
-         li.appendChild(editBtn)
-         list.appendChild(li)
-       }; 
-      console.log(e.data)
-   })
-   .catch((err) =>{
-      console.log(err)
-   })
-
+         showonScreen(arr[i])
+      }; 
+      })
+      .catch((err) =>{
+         console.log(err)
+      })
    
+
+
+
+
    
